5aeffe1d272c85d7f1e57ead918501f575f1f481
Dulal-Ahmed <dulalaahmedjalal@gmail.com>
2023-02-01 02:40:19 +0600

    ami to bala nai

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function myFunction(x) {
   x.classList.toggle("change");
   mobileMenu.classList.toggle("menuChange");
 }
+
+// text animation
+window.addEventListener('scroll', reveal);
+
+      function reveal(){
+          let textAnimationContainer = document.querySelectorAll('.textAnimationContainer');
+         let clipPathh2 = document.querySelectorAll('.clipPathh2');
+      for(let i=0; i< textAnimationContainer.length; i++){  
+        let windowheight = window.innerHeight;
+        let revealtop = textAnimationContainer[i].getBoundingClientRect().top;
+        let revealpoint = 50;
+
+        if(revealtop < windowheight - revealpoint){
+         clipPathh2[i].style.clipPath = 'polygon(96% 100%, 98% 95%, 100% 82%, 99% 49%, 100% 44%, 97% 1%, 0 4%, 1% 23%, 0 85%, 0 99%)';
+         console.log(textAnimationContainer[3].style.clipPath);
+     }
+
+      }
+     }
