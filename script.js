c6e2f2e441f1ff19486988e23e7f29d859d4396e
Dulal-Ahmed <dulalaahmedjalal@gmail.com>
2023-02-01 08:46:18 +0600

    new update

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ window.addEventListener('scroll', reveal);
       function reveal(){
           let textAnimationContainer = document.querySelectorAll('.textAnimationContainer');
          let clipPathh2 = document.querySelectorAll('.clipPathh2');
+      
       for(let i=0; i< textAnimationContainer.length; i++){  
         let windowheight = window.innerHeight;
         let revealtop = textAnimationContainer[i].getBoundingClientRect().top;
@@ -18,7 +19,7 @@ window.addEventListener('scroll', reveal);
 
         if(revealtop < windowheight - revealpoint){
          clipPathh2[i].style.clipPath = 'polygon(96% 100%, 98% 95%, 100% 82%, 99% 49%, 100% 44%, 97% 1%, 0 4%, 1% 23%, 0 85%, 0 99%)';
-         console.log(textAnimationContainer[3].style.clipPath);
+      
      }
 
       }
