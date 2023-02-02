39609dfbd90bbf201d7124ffea5b10467de72981
Dulal-Ahmed <dulalaahmedjalal@gmail.com>
2023-02-02 20:01:00 +0600

    update complete

--- script.js
+++ script.js
@@ -20,17 +20,30 @@ window.addEventListener('scroll', reveal);
 
       function reveal(){
           let textAnimationContainer = document.querySelectorAll('.textAnimationContainer');
-         let clipPathh2 = document.querySelectorAll('.clipPathh2');
+          let svgAnimationContainer = document.querySelectorAll('.svgAnimationContainer');
+        let clipPathh2 = document.querySelectorAll('.clipPathh2');
+        let clipPathh3 = document.querySelectorAll('.clipPathh3');
       
       for(let i=0; i< textAnimationContainer.length; i++){  
         let windowheight = window.innerHeight;
         let revealtop = textAnimationContainer[i].getBoundingClientRect().top;
-        let revealpoint = 50;
+      let revealpoint = 50;
 
         if(revealtop < windowheight - revealpoint){
          clipPathh2[i].style.clipPath = 'polygon(96% 100%, 98% 95%, 100% 82%, 99% 49%, 100% 44%, 97% 1%, 0 4%, 1% 23%, 0 85%, 0 99%)';
       
      }
+    }
+      for(let i=0; i< svgAnimationContainer.length; i++){  
+        let windowheight = window.innerHeight;
+        let revealtop = svgAnimationContainer[i].getBoundingClientRect().top;
+      let revealpoint = 50;
+
+        if(revealtop < windowheight - revealpoint){
+         clipPathh3[i].style.clipPath = 'polygon(96% 100%, 98% 95%, 100% 82%, 99% 49%, 100% 44%, 97% 1%, 0 4%, 1% 23%, 0 85%, 0 99%)';
+      
+     }
+
 
       }
      }
